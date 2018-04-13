7a545eb118239dedee73cd7d41cf17a189181360
Benjamin Sykes <33205078+sykeben@users.noreply.github.com>
2018-04-13 10:58:17 -0400

    Added math commands.
    Thank you to Nicholis Hubbard for helping me!

--- main.py
+++ main.py
@@ -8,6 +8,7 @@
 # <<< Import libraries and custom modules >>>
 import cmd
 import os
+import math
 
 # <<< Define the "SykeShell" class >>>
 class SykeShell(cmd.Cmd):
@@ -23,7 +24,44 @@ class SykeShell(cmd.Cmd):
         try:
             os.system("cls")
         except:
-            os.system("clear")
+            try:
+                os.system("clear")
+            except:
+                print("An error occurred.")
+
+    # ~~~ Basic commands ~~~
+    def do_beep(self, arg):
+        print('\b',end='')
+    def do_print(self, arg):
+        print(arg)
+
+    # ~~~ Mathematical commands ~~
+    def do_math(self, arg):
+        arglist = arg.split()
+        mathmode = arglist[0]
+        try:
+            if mathmode == "add":
+                print(int(arglist[1]) + int(arglist[2]))
+            elif mathmode == "subtract":
+                print(int(arglist[1]) - int(arglist[2]))
+            elif mathmode == "multiply":
+                print(int(arglist[1]) * int(arglist[2]))
+            elif mathmode == "divide":
+                print(int(arglist[1]) / int(arglist[2]))
+            elif mathmode == "modulo":
+                print(int(arglist[1]) % int(arglist[2]))
+            elif mathmode == "sin":
+                print(math.sin(float(arglist[1])))
+            elif mathmode == "cos":
+                print(math.cos(float(arglist[1])))
+            elif mathmode == "tan":
+                print(math.tan(float(arglist[1])))
+            elif mathmode == "sqrt":
+                print(math.sqrt(float(arglist[1])))
+            else:
+                print("Invalid operation.")
+        except ValueError:
+            print("Invalid type or argument count.")
 
 if __name__ == '__main__':
     SykeShell().cmdloop()
