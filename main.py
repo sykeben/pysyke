cfc5a44667a7086d618cc92333524050b1c613bb
Benjamin Sykes <33205078+sykeben@users.noreply.github.com>
2018-04-13 10:19:37 -0400

    Updated

--- main.py
+++ main.py
@@ -5,3 +5,25 @@
 # Ben Sykes              #
 ##########################
 
+# <<< Import libraries and custom modules >>>
+import cmd
+import os
+
+# <<< Define the "SykeShell" class >>>
+class SykeShell(cmd.Cmd):
+    intro = 'Welcome to SykeShell, the shell for PySyke\n'
+    prompt = '>-=> '
+    file = None
+
+    # ~~~ Core commands ~~~
+    def do_quit(self, arg):
+        quit()
+    def do_clear(self, arg):
+        # Try to clear the screen
+        try:
+            os.system("cls")
+        except:
+            os.system("clear")
+
+if __name__ == '__main__':
+    SykeShell().cmdloop()
